b468ad8b4a38670e9288ad1ea8ff536327d09ec2
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2018-11-06 11:16:49 +0700

    data mock added

--- src/App.js
+++ src/App.js
@@ -4,29 +4,36 @@ import "./App.css";
 
 import { fetchPDC } from "./controllers/pdc";
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			x: []
+		};
+	}
 	async fetchData() {
 		console.log(await fetchPDC());
 	}
 	render() {
 		this.fetchData();
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-				</header>
-			</div>
-		);
+		return <div />;
+		// return (
+		// 	<div className="App">
+		// 		<header className="App-header">
+		// 			<img src={logo} className="App-logo" alt="logo" />
+		// 			<p>
+		// 				Edit <code>src/App.js</code> and save to reload.
+		// 			</p>
+		// 			<a
+		// 				className="App-link"
+		// 				href="https://reactjs.org"
+		// 				target="_blank"
+		// 				rel="noopener noreferrer"
+		// 			>
+		// 				Learn React
+		// 			</a>
+		// 		</header>
+		// 	</div>
+		// );
 	}
 }
 
